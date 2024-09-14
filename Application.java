342d24da94bc46aae37a8ec79a280786faa78e92
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2024-09-14 07:13:30 +0530

    newfeature 2.2 changes by raja

--- Application.java
+++ Application.java
@@ -12,7 +12,25 @@ class Main {
     // take integer input from the user
     System.out.println("Enter a number");
     int number = input.nextInt();
-	   
+	  //newfeature 2.2 changes done by raja in oct 2023 
+    sb.append(')');
+            Type[] exceptions = getGenericExceptionTypes();
+            if (exceptions.length > 0) {
+                sb.append(" throws ");
+                for (int k = 0; k < exceptions.length; k++) {
+                    sb.append((exceptions[k] instanceof Class)?
+                              ((Class)exceptions[k]).getName():
+                              exceptions[k].toString());
+                    if (k < (exceptions.length - 1))
+                        sb.append(',');
+                }
+            }
+            return sb.toString();
+        } catch (Exception e) {
+            return "<" + e + ">";
+        }
+    }
+    //end of newfeature 2.2 
     // while loop continues 
     // until entered number is positive
     while (number >= 0) {
